52ab396a24fa5916b86ea6c46f7a630d0f943de6
darshangit <darshans1989@gmail.com>
2018-04-29 12:27:16 +0530

    minor changes: Proprties comfig

--- src/app/properties-config/properties-config.component.ts
+++ src/app/properties-config/properties-config.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ServiceRestartService } from '../services/service-restart.service';
 import { SelectItem } from 'primeng/primeng';
+import { isNullOrUndefined } from 'util';
 
 @Component({
   selector: 'app-properties-config',
@@ -10,6 +11,8 @@ import { SelectItem } from 'primeng/primeng';
 export class PropertiesConfigComponent implements OnInit {
 
   services: SelectItem[];
+  selectedProperties: SelectItem[];
+
   selectedService: string;
 
   selectedEnvironment: string;
@@ -36,6 +39,28 @@ export class PropertiesConfigComponent implements OnInit {
   addProperty() {
     this.displayProperty = true;
     this.newProperty = {};
+
+    if (!isNullOrUndefined(this.key) && !isNullOrUndefined(this.value)) {
+      const addItem: SelectItem = {
+        label: this.key,
+        value: this.value
+      };
+      this.services.push(addItem);
+      this.displayProperty = false;
+      this.key = null;
+      this.value = null;
+    }
+  }
+
+  closeProperty() {
+    this.displayProperty = false;
+    this.key = null;
+      this.value = null;
+  }
+
+  update() {
+    console.log('this.services', this.services);
+    console.log('this.selectedProperties', this.selectedProperties);
   }
 
 }
